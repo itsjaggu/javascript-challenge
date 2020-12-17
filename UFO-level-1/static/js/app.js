469d0d9daddbbea459a986ad02f9cd1c3663ae79
Jagrati Joshi <jagrati.joshi@outlook.com>
2020-12-17 16:14:14 -0700

    appended data to html table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,17 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// appending data to html table
+tableData.forEach((ufoRow) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoRow).forEach(([key, value]) => {
+        if (key != "durationMinutes") {
+            var cell = row.append("td");
+            cell.text(value);
+        }
+    });
+  });
+  
